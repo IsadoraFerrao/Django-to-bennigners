14266f068e72dc1351b8f143c40f4b30e1d6f17a
IsadoraFerrao <isadora-gf@hotmail.com>
2022-06-08 19:43:17 -0300

    aula

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BolsistasConfig(AppConfig):
+class BolsistaswomakersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bolsistas'
+    name = 'bolsistasWoMakers'
